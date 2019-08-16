73ba787e166f1404680089bd46bab3297cdb2b19
张永杰 <zhangyongjie@edaijia-inc.cn>
2019-08-16 18:03:53 +0800

    css

---
+++ src/index/app.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  #container.container Hello {{bundler}}!
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      bundler: 'Parcel'
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.container {
+  color: green;
+}
+</style>
